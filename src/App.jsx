83b546cca95270f1c5384d726a5365761bedfbbc
alikyadav111 <164210649+alikyadav111@users.noreply.github.com>
2024-05-18 15:18:50 +0530

    frest cumiit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <table>
+    <thead>
+      <tr>
+        <th>id</th>
+      </tr>
+      <td></td>
+    </thead>
+     </table>
+     </>
+  )
+}
+
+export default App
