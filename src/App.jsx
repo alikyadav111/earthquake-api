6375ca379d06a27b694039d429c8eaa22217f7d3
Kalal mukesh <105076417+Mukeshkalal@users.noreply.github.com>
2024-05-18 16:41:23 +0530

    Commit remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,42 @@
-<<<<<<< HEAD
-=======
-import './App.css'
-
->>>>>>> 3e9d5b06395b5a5ce56bf9913b145db5f1405e1a
 function App() {
 
   return (
-    <>
-<<<<<<< HEAD
-      <table>
-        <thead>
-          <tr>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-=======
-      <table border='2'>
-        <thead>
-          <tr>
-            <th>Sr no</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Password</th>
->>>>>>> 3e9d5b06395b5a5ce56bf9913b145db5f1405e1a
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-<<<<<<< HEAD
-            <td>qw</td>
-            <td>qw</td>
-            <td>qw</td>
-=======
-            <td>1</td>
-            <td>mukesh</td>
-            <td>mukesh@gmail.com</td>
-            <td>123123</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>alik</td>
-            <td>alik@gmail.com</td>
-            <td>124124</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>kisan</td>
-            <td>kisan@gmail.com</td>
-            <td>@sadnfd</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Suraj</td>
-            <td>Suraj@gmail.com</td>
-            <td>@sadnfd</td>
->>>>>>> 3e9d5b06395b5a5ce56bf9913b145db5f1405e1a
-          </tr>
-        </tbody>
-      </table>
+    <>      <table border='2'>
+      <thead>
+        <tr>
+          <th>Sr no</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>mukesh</td>
+          <td>mukesh@gmail.com</td>
+          <td>123123</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>alik</td>
+          <td>alik@gmail.com</td>
+          <td>124124</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>kisan</td>
+          <td>kisan@gmail.com</td>
+          <td>@sadnfd</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Suraj</td>
+          <td>Suraj@gmail.com</td>
+          <td>@sadnfd</td>
+        </tr>
+      </tbody>
+    </table>
     </>
   )
 }
